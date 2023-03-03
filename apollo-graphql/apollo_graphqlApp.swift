9eb3a8d4d9b60a8c6cab22be29441f50a2254b85
Ali Rıza Reisoğlu <alirizareisoglu@gmail.com>
2023-03-03 12:57:11 +0300

    Initial Commit

---
+++ apollo-graphql/apollo_graphqlApp.swift
@@ -0,0 +1,17 @@
+//
+//  apollo_graphqlApp.swift
+//  apollo-graphql
+//
+//  Created by Ali Riza Reisoglu on 3.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct apollo_graphqlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
